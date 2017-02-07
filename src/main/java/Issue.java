0a30c4ff4fb455cb0daec730988b5608f0228aab
harshinimitta <sivaharshinimitta@gmail.com>
2017-02-07 00:21:25 -0500

    Added properties and their getter setter methods for Issue and User class

--- src/main/java/Issue.java
+++ src/main/java/Issue.java
@@ -1,5 +1,72 @@
 package main.java;
 
+import java.util.Date;
+
 public class Issue {
 
+    private int number;
+    private int id;
+    private String state;
+    private String title;
+    private String body;
+    private Date createdAt;
+    private Date closedAt;
+    private User user;
+    private User assignee;
+    
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Date getClosedAt() {
+        return closedAt;
+    }
+    public void setClosedAt(Date closedAt) {
+        this.closedAt = closedAt;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public User getAssignee() {
+        return assignee;
+    }
+    public void setAssignee(User assignee) {
+        this.assignee = assignee;
+    }
+    
 }
